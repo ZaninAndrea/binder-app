87d20a9cc59511d055dd953fea8eb8a24be35beb
ZaninAndrea <99.zanin@gmail.com>
2019-08-30 23:34:16 +0200

    Updates

---
+++ precache-manifest.690a91282f61e9fd42b8c0d3c8927d4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3700c079bd55b85ba0c4e9f48501ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db819842989557cc645e",
+    "url": "/static/css/main.fd61d849.chunk.css"
+  },
+  {
+    "revision": "2cc7aa87946b0863183a",
+    "url": "/static/js/2.92d97d23.chunk.js"
+  },
+  {
+    "revision": "db819842989557cc645e",
+    "url": "/static/js/main.9e7ed44f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
